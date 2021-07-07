969cbada2920a7c84df23ef67ca8412bfc9e91b6
Simon Henke <h3.simonh@yahoo.de>
2021-07-07 23:01:50 +0200

    split scale into x and y and add lock ratio toggle

--- index.js
+++ index.js
@@ -1,7 +1,77 @@
-function scaleLayers() {
-  const app = window.require("photoshop").app;
-  const activeLayers = app.activeDocument.activeLayers;
+const scaleInputX = document.getElementById("scaleInputX");
+const scaleInputY = document.getElementById("scaleInputY");
+const lockRatioCb = document.getElementById("lockRatioCb");
 
+let scaleX, scaleY, lockRatio;
+const app = window.require("photoshop").app;
+
+loadSettings();
+
+scaleInputX.addEventListener("input", () => {
+  if (lockRatio) {
+    scaleInputY.value = format(scaleInputX.value);
+  }
+  updateAndSaveSettings();
+});
+
+scaleInputY.addEventListener("input", () => {
+  if (lockRatio) {
+    scaleInputX.value = format(scaleInputY.value);
+  }
+  updateAndSaveSettings();
+});
+
+lockRatioCb.addEventListener("change", updateAndSaveSettings);
+
+document
+  .getElementById("btnPopulate")
+  .addEventListener("click", () => scaleLayers(scaleX, scaleY));
+
+
+/* Loads the settings and updates the input values */
+async function loadSettings() {
+  const settingsStr = await window.localStorage.getItem("settings");
+  if (settingsStr) {
+    const settings = JSON.parse(settingsStr);
+    ({ scaleX, scaleY, lockRatio } = settings);
+  }
+  scaleInputX.value = format(scaleX || scaleInputX.value);
+  scaleInputY.value = format(scaleY || scaleInputY.value);
+  lockRatioCb.checked =
+    typeof lockRatio === undefined ? lockRatioCb.checked : lockRatio;
+}
+
+/* Fix float number localization: 1,5 -> 1.5 */
+function format(str) {
+  return str.replace(",", ".");
+}
+
+/* Set the constants to the current input values and 
+ * save them to the local storage */
+async function updateAndSaveSettings() {
+  scaleX = format(scaleInputX.value);
+  scaleY = format(scaleInputY.value);
+  lockRatio = lockRatioCb.checked;
+  window.localStorage.setItem(
+    "settings",
+    JSON.stringify({
+      scaleX,
+      scaleY,
+      lockRatio,
+    })
+  );
+}
+
+/* Scale the active layers relative to their position */
+function scaleLayers(x, y) {
+  if (!x || !y) {
+    app.showAlert("The scales should be at least 1%");
+    return;
+  }
+  x = Number(x);
+  y = Number(y);
+
+  const activeLayers = app.activeDocument.activeLayers;
   const allLayers = app.activeDocument.layers;
 
   if (activeLayers.length < 2) {
@@ -9,52 +79,24 @@ function scaleLayers() {
     return;
   }
 
-  const scale = Number(document.getElementById("scaleInput").value);
-  if (scale !== 100) {
-    // allLayers.forEach(layer => {
-    //   layer.selected = false
-    // });
+  if (x !== 100 || y !== 100) {
     selectLayers(allLayers, false);
 
     activeLayers.forEach((layer) => {
       layer.selected = true;
-      layer.scale(scale, scale);
+      layer.scale(x, y);
       layer.selected = false;
     });
 
-    // activeLayers.forEach((layer) => {
-    //   layer.selected = true;
-    // });
     selectLayers(activeLayers, true);
   } else {
     app.showAlert("Please select any scale other than 100%");
   }
 }
 
+/* select or deselect specified layers */
 function selectLayers(layers, select) {
   layers.forEach((layer) => {
     layer.selected = select;
   });
 }
-
-document.getElementById("btnPopulate").addEventListener("click", scaleLayers);
-
-const scaleInput = document.getElementById("scaleInput");
-
-scaleInput.value = window.localStorage.getItem("scale") || scale.value;
-scaleInput.addEventListener("change", () =>
-  window.localStorage.setItem("scale", scaleInput.value)
-);
-
-
-/*
-
-Tasks:
-- Split up scale into X and Y, add a "locked" toggle 
-(when active, update values of both inputs simultaneously)
-
-- Instead of setting scale via input, listen to scale event and repeat it for all other layers
-
-- BatchPlay: Convert to Smart Object before scale
-
-*/
