b562035c2e53779aa632268e995c5a8acab5c1a2
Simon Henke <h3.simonh@yahoo.de>
2021-07-17 15:49:02 +0200

    validate the input and notify the user

--- index.js
+++ index.js
@@ -7,8 +7,16 @@ const sizeInput = document.getElementById("sizeInput");
 function scaleLayers() {
   const app = window.require("photoshop").app;
   const activeLayers = app.activeDocument.activeLayers;
+  if(!activeLayers.length) {
+    app.showAlert("Please select at least one layer")
+    return
+  }
   const size = Number(sizeInput.value)
 
+  if(size === 100) {
+    app.showAlert("Please select a different size than 100%")
+    return
+  }
   activeLayers.forEach(layer => layer.selected = false)
 
   activeLayers.forEach(layer => {
