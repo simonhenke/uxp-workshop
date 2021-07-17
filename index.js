2b38103c713935c8fe06012f692460bed7df937e
Simon Henke <h3.simonh@yahoo.de>
2021-07-17 15:40:41 +0200

    add Spectrum textfield to allow user input

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
+document
+  .getElementById("btnScale")
+  .addEventListener("click", scaleLayers);
+
+const sizeInput = document.getElementById("sizeInput");
+
 function scaleLayers() {
   const app = window.require("photoshop").app;
   const activeLayers = app.activeDocument.activeLayers;
+  const size = Number(sizeInput.value)
 
   activeLayers.forEach(layer => layer.selected = false)
 
   activeLayers.forEach(layer => {
     layer.selected = true;
-    layer.scale(50,50);
+    layer.scale(size,size);
     layer.selected = false;
   })
 
   activeLayers.forEach(layer => layer.selected = true)
 }
-
-document
-  .getElementById("btnScale")
-  .addEventListener("click", scaleLayers);
