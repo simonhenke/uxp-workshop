cca5a84cc0773bd907a5275e6646a62945b6801a
Simon Henke <h3.simonh@yahoo.de>
2021-07-17 15:12:34 +0200

    scale layers using the Photshop API

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 function scaleLayers() {
   const app = window.require("photoshop").app;
+  const activeLayers = app.activeDocument.activeLayers;
+
+  activeLayers.forEach(layer => layer.selected = false)
+
+  activeLayers.forEach(layer => {
+    layer.selected = true;
+    layer.scale(50,50);
+    layer.selected = false;
+  })
+
+  activeLayers.forEach(layer => layer.selected = true)
 }
 
 document
