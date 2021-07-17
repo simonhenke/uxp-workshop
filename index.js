ccb26510799892f93e01847a79e5d8c0e2d5a515
Simon Henke <h3.simonh@yahoo.de>
2021-07-17 14:56:54 +0200

    tidy up and remove unused code

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-function showLayerNames() {
-    const app = window.require("photoshop").app;
-    const allLayers = app.activeDocument.layers;
-    const allLayerNames = allLayers.map(layer => layer.name);
-    const sortedNames = allLayerNames.sort((a, b) => a < b ? -1 : a > b ? 1 : 0);
-    document.getElementById("layers").innerHTML = `
-      <ul>${
-        sortedNames.map(name => `<li>${name}</li>`).join("")
-      }</ul>`;
+function scaleLayers() {
+  const app = window.require("photoshop").app;
 }
 
-document.getElementById("btnPopulate").addEventListener("click", showLayerNames);
+document
+  .getElementById("btnScale")
+  .addEventListener("click", scaleLayers);
