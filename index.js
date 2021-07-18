42820601755475fd37dd906373ec11b5fee14d88
Simon Henke <h3.simonh@yahoo.de>
2021-07-18 20:15:39 +0200

    add comments and fix duplicate events

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ action.addNotificationListener([{event: 'transform'}], onTransformEvent);
 let syncLayers = []
 loadSyncLayers()
 
+/* Load the layers to synchronize from localStorage */
 async function loadSyncLayers() {
   const str = await localStorage.getItem("syncLayers")
   if(str) {
@@ -13,18 +14,24 @@ async function loadSyncLayers() {
   }
 }
 
+/* Handler getting called on every transform event.
+/* Repeat the transformation for all sync-layers except the one already transformed. */
 function onTransformEvent(eventName, descriptor) {
+  if(!descriptor._isCommand){
+    return
+  }
   const doc = app.activeDocument
   const activeLayers = doc.activeLayers
   const currentIds = doc.activeLayers.map(l => l._id)  
   const syncIds = syncLayers.map(l => l._id)
+  // filter out the layers on which the transformation was just done
   const otherLayers = doc.layers.filter(layer => syncIds.includes(layer._id) && !currentIds.includes(layer._id))
-  selectLayers(doc.layers, false);
+  selectLayers(doc.activeLayers, false);
   otherLayers.forEach(layer => {
     const transformDescriptor = {
-      _obj: 'transform',
-      ...descriptor,
-      _target: {_ref: 'layer', _id: layer._id}
+      _obj: 'transform', // add event name as it isn't included in the descriptor
+      ...descriptor, // take all props from the descriptor
+      _target: {_ref: 'layer', _id: layer._id} // overwrite the target to point to another layer
     }
     layer.selected = true
     action.batchPlay([transformDescriptor], {synchronousExecution: true})
@@ -33,6 +40,7 @@ function onTransformEvent(eventName, descriptor) {
   selectLayers(activeLayers, true)
 }
 
+/* Set the sync-layers to the currently selected ones (and updates the view) */
 function setSyncLayers() {
   syncLayers = app.activeDocument.activeLayers.map(layer => ({
     _id: layer._id,
@@ -42,6 +50,7 @@ function setSyncLayers() {
   localStorage.setItem("syncLayers", JSON.stringify(syncLayers))
 }
 
+/* Display the names of the sync-layers in a list */
 function updateView() {
   document.getElementById("layers").innerHTML = syncLayers.length ? `
   <ul>${
@@ -49,6 +58,7 @@ function updateView() {
   }</ul>`: 'No layers';
 }
 
+/* Helper function to select or deselect layers*/
 function selectLayers(layers, select) {
   layers.forEach((layer) => {
     layer.selected = select;
